568d126651263a59da42b7730462b5ee6168e1a3
lucieprazakova <lucka.prazakova@gmail.com>
2024-10-12 14:50:08 +0200

    minor changes

---
+++ lambda_functions/lambda_02-Copy_S3_file.py
@@ -0,0 +1,24 @@
+#### Lambda function test #### 
+
+# import libraries 
+import boto3
+
+s3_client = boto3.client('S3')
+
+# def function
+def lambda_handler(event, context):
+    # Source bucket
+    source_bucket = event['Records'][0]['s3']['bucket']['name']
+     #F Fle key in source bucket
+    source_key = event['Records'][0]['s3']['bucket']['name']
+
+    # Destination bucekt
+    destination_bucket = 'target_bucket'
+
+    # Copy object to the destination bucket
+    s3_client.copy_object(Bucket = destination_bucket,
+                          CopySource={'Bucket', source_bucket, 'Key': source_key},
+                          Key = source_key)
+    
+    # Optional - delete the file from the source bucket after copying
+    # s3_client.delete_object(Bucket=source_bucket, Key=source_key)
